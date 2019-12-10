b7711f88a0033a50a6d49b1a60d3e36ade4c68f9
Abdülbaki Kaplan <abdulbakikaplan@hotmail.com>
2019-12-10 10:21:17 +0300

    Initial Commit

---
+++ KukaAppsCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KukaAppsCase
+//
+//  Created by Abdülbaki Kaplan on 12/10/19.
+//  Copyright © 2019 Abdülbaki Kaplan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
